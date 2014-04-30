926fefe4cfa6b33ab3149c912e7ea58789acafca
Vinicius Linck <vinnylinck@gmail.com>
2014-04-29 23:58:42 -0300

    added new libs

--- www/js/views/home.js
+++ www/js/views/home.js
@@ -4,13 +4,16 @@
 
     var drawHeader = function (element) {
         element.firstElementChild.innerHTML = "Listagram";
-    };
+    },
+        
+        drawWall = function (element) {
+        };
 
     // declaring view
     n.view = function (ctrl) {
         return m('', [
             m('x-appbar#homeBar.redbar', {config: drawHeader}),
-            m('', 'content')
+            m('','content')
         ]);
     };
 
@@ -21,3 +24,17 @@
 
 // loader
 // http://codepen.io/NeekGerd/pen/jnkGy
+//http://damien.antipa.at/2012/10/16/ios-pull-to-refresh-in-mobile-safari-with-native-scrolling/
+/*
+   m('', {config: drawWall}, [
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})]),
+                m('', [m('img', {src: 'http://placehold.it/100x100'})])
+            ])*/
